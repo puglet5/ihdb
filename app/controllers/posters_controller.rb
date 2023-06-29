6c99f0e18d920c8dd7e252ad4299c22aecc18104
Michael Basmanov <puglet5@mail.ru>
2023-06-29 19:21:27 +0300

    feat: posters controller draft

---
+++ app/controllers/posters_controller.rb
@@ -0,0 +1,79 @@
+# frozen_string_literal: true
+
+class PostersController < ApplicationController
+  before_action :set_poster, only: %i[show edit update destroy]
+
+  breadcrumb 'Home', :root
+  breadcrumb 'Posters', :posters, match: :exclusive
+
+  def index
+    posters = Poster.all.order('created_at asc')
+
+    @query = posters.ransack(params[:query])
+    @pagy, @posters = pagy @query.result(distinct: true).order('created_at DESC'), items: 20
+  end
+
+  def show
+    @poster = Poster.with_attached_images
+                    .find(params[:id])
+
+    respond_to do |format|
+      format.html do
+        breadcrumb @poster.title, @poster, match: :exclusive
+      end
+    end
+  end
+
+  def new
+    @poster = current_user.posters.build poster_params
+
+    breadcrumb 'New Poster', %i[new poster], match: :exclusive
+  end
+
+  def edit
+    breadcrumb @poster.title, @poster, match: :exclusive
+    breadcrumb 'Edit', [:edit, @poster], match: :exclusive
+  end
+
+  def create
+    @poster = current_user.posters.build(poster_params)
+
+    if @poster.save
+      redirect_to @poster
+      flash[:success] = 'Poster added!'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @poster.update
+      redirect_to @poster
+      flash[:success] = 'Poster updated!'
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  rescue ActiveRecord::StaleObjectError
+    flash[:error] = 'Error! Poster was modified elsewhere.'
+    redirect_to [:edit, @poster]
+  end
+
+  def destroy
+    @poster.destroy
+    flash[:success] = 'Poster deleted!'
+    redirect_to posters_url, status: :see_other
+  end
+
+  private
+
+  def set_poster
+    @poster = Poster.find(params[:id])
+  end
+
+  def poster_params
+    params.fetch(:poster, {}).permit(
+      :title,
+      images: []
+    )
+  end
+end
