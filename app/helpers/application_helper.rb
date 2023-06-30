bfaaccdc898f0b9113e0bcdb6348ebf9049dfb4b
Michael Basmanov <puglet5@mail.ru>
2023-06-30 22:17:04 +0300

    feat[ui]: fix default blob partial, show original image if no variant exists

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,12 @@
 
 module ApplicationHelper
   include Pagy::Frontend
+
+  def original_or_variant(image, variant)
+    if variant.key
+      variant.processed.url if image.service.exist?(variant.key)
+    else
+      image.url
+    end
+  end
 end
