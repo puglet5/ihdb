58a24703755c44afeafa07e5f1fb2f98fb046e92
Michael Basmanov <puglet5@mail.ru>
2023-07-01 03:07:28 +0300

    chore: uppy controller refactor

--- app/javascript/controllers/uppy_controller.ts
+++ app/javascript/controllers/uppy_controller.ts
@@ -1,6 +1,6 @@
 import { Controller } from "@hotwired/stimulus"
-import Uppy, { PluginOptions, UppyFile } from "@uppy/core"
-import Dashboard from "@uppy/dashboard"
+import Uppy, { PluginOptions, UppyFile, UppyOptions } from "@uppy/core"
+import Dashboard, { DashboardOptions } from "@uppy/dashboard"
 import ActiveStorageUpload from "uppy-activestorage-upload"
 
 export default class extends Controller {
@@ -21,48 +21,48 @@ export default class extends Controller {
   readonly textTarget!: HTMLElement
   readonly divTarget!: HTMLElement
 
-  readonly hasTriggerTarget: boolean
-  readonly hasDivTarget: boolean
-  readonly hasTextTarget: boolean
-
   connect() {
+
+    const uppyOptions: UppyOptions = {
+      autoProceed: false,
+      allowMultipleUploads: this.allowmultiplefilesValue,
+      allowMultipleUploadBatches: this.allowmultiplefilesValue,
+      restrictions: {
+        allowedFileTypes: this.allowedfiletypesValue.length ? this.allowedfiletypesValue : null,
+        maxNumberOfFiles: this.allowmultiplefilesValue ? null : 1
+      },
+    }
+
+    const dashboardOptions: DashboardOptions = {
+      disableThumbnailGenerator: !this.generatethumbnailsValue,
+      // @ts-expect-error
+      trigger: this.triggerTarget,
+      doneButtonHandler: null,
+      target: this.divTarget,
+      closeModalOnClickOutside: true,
+      animateOpenClose: false,
+      disableInformer: true,
+      theme: "auto",
+      hidePauseResumeButton: true,
+      closeAfterFinish: false,
+      inline: false,
+      showRemoveButtonAfterComplete: false,
+      showProgressDetails: true,
+      fileManagerSelectionType: "files",
+      proudlyDisplayPoweredByUppy: false
+    }
 
     const setupUppy = (element: HTMLElement) => {
       const directUploadUrl = document.querySelector("meta[name='direct-upload-url']").getAttribute("content")
       const fieldName = element.dataset.uppy
 
-      const uppy = new Uppy({
-        autoProceed: false,
-        allowMultipleUploads: this.allowmultiplefilesValue,
-        allowMultipleUploadBatches: this.allowmultiplefilesValue,
-        restrictions: {
-          allowedFileTypes: this.allowedfiletypesValue.length ? this.allowedfiletypesValue : null,
-          maxNumberOfFiles: this.allowmultiplefilesValue ? null : 1
-        },
-      })
+      const uppy = new Uppy(uppyOptions)
 
       uppy.use(ActiveStorageUpload, {
         directUploadUrl
       } as PluginOptions)
 
-      uppy.use(Dashboard, {
-        disableThumbnailGenerator: !this.generatethumbnailsValue,
-        // @ts-expect-error
-        trigger: this.triggerTarget,
-        doneButtonHandler: null,
-        target: this.divTarget,
-        closeModalOnClickOutside: true,
-        animateOpenClose: false,
-        disableInformer: true,
-        theme: "auto",
-        hidePauseResumeButton: true,
-        closeAfterFinish: false,
-        inline: false,
-        showRemoveButtonAfterComplete: false,
-        showProgressDetails: true,
-        fileManagerSelectionType: "files",
-        proudlyDisplayPoweredByUppy: false
-      })
+      uppy.use(Dashboard, dashboardOptions)
 
       uppy.on("complete", (result) => {
         const filecountText = document.querySelector(`#${this.textTarget.id}`) as HTMLElement
