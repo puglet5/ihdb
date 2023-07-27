1b39134071c1376f4e10f73a136044f372b47f05
Michael Basmanov <puglet5@mail.ru>
2023-07-27 14:02:08 +0300

    fix: add user name to registration form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,7 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     update_attrs = %i[password password_confirmation
-                      current_password]
+                      current_password first_name last_name]
     devise_parameter_sanitizer.permit :account_update, keys: update_attrs
     devise_parameter_sanitizer.permit :sign_up, keys: update_attrs
     devise_parameter_sanitizer.permit :sign_in, keys: %i[login password]
