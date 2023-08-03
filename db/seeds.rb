e36b8f0df2caeee8f55cb77bd5225c895f7ed387
Michael Basmanov <puglet5@mail.ru>
2023-08-03 14:19:25 +0300

    dev: add optional poster attributes to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,11 +3,34 @@
 require 'faker'
 
 10.times do
-  User.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: Faker::Internet.email, password: '123456', password_confirmation: '123456')
+  Locality.create!(
+    name: Faker::Address.city
+  )
 end
 
 10.times do
-  p = Poster.new(title: Faker::Book.title, user: User.all.sample)
+  User.create!(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    email: Faker::Internet.email,
+    password: '123456',
+    password_confirmation: '123456'
+  )
+end
+
+10.times do
+  p = Poster.new(
+    title: Faker::Book.title,
+    user: User.all.sample,
+    locality: Locality.all.sample,
+    owner: Faker::Name.name,
+    acquisition_date: Faker::Date.in_date_period,
+    event_datetime: Faker::Time.between(from: DateTime.now - 1, to: DateTime.now),
+    category: 0,
+    status: 0,
+    condition: 0,
+    sku: Faker::Barcode.ean
+  )
   i = Image.new(imageable: p)
   i.image.attach(io: File.open('spec/fixtures/files/test.jpeg'), filename: 'image.jpeg', content_type: 'image/jpeg')
   i.save!
